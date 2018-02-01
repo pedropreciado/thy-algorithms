195f551dba6aede090a1727b98083956ccb73aec
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-01 00:18:37 -0800

    create vertex

---
+++ graph/edge.js
@@ -0,0 +1,7 @@
+class Edge {
+
+  constructor(fromVertex) {
+
+  }
+
+}
---
+++ graph/vertex.js
@@ -0,0 +1,11 @@
+class Vertex {
+
+  constructor(value, inEdges, outEdges) {
+    this.value = value;
+    this.inEdges = inEdges;
+    this.outEdges = outEdges;
+  }
+
+}
+
+module.exports = Vertex;
