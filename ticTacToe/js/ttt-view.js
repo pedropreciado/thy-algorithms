9e9692adbbe8b615fea88523fd0239e3faacb3ba
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-19 00:32:05 -0800

    arrlen

--- ticTacToe/js/ttt-view.js
+++ ticTacToe/js/ttt-view.js
@@ -4,6 +4,7 @@ class View {
     this.board = $el;
 
     this.setupBoard();
+    this.prettyIntro();
   }
 
   bindEvents() {}
@@ -19,12 +20,38 @@ class View {
         let $li = $("<li>");
 
         $li.data("pos", [rowIdx, colIdx]);
+        $li.addClass("square");
 
         $ul.append($li)
       }
     }
 
     this.board.append($ul);
+  }
+
+  prettyIntro() {
+    let colors = ["#1A1a1D", "#4E4E50", "#6F2232", "#950740", "#C3073F"];
+
+    setTimeout(() => {
+
+      $(".square").each((idx, el) => {
+        let randomColor = '#' + Math.random().toString(16).substr(-6);
+        let $el = $(el);
+
+        $el.css("background-color", randomColor);
+        $el.css("transition", "1s");
+      });
+
+    }, 500)
+
+    setTimeout(() => {
+      $(".square").each((idx, el) => {
+        let $el = $(el);
+
+        $el.css("background-color", "#4E4E50");
+        $el.css("transition", "1s");
+      });
+    }, 1200)
   }
 }
 
