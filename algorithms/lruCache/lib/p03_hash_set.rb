eab18921d82c98d4701a94dd2e845bd9a562ef37
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-26 12:14:18 -0800

    hash set

--- algorithms/lruCache/lib/p03_hash_set.rb
+++ algorithms/lruCache/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      resize! if self.count == num_buckets - 1
+
+      self[key].push(key)
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    hash = key.hash
+    @store[hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,15 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    old_store = @store
+
+    old_store.each do |bucket|
+      bucket.each do |key|
+          new_store[key.hash % new_store.length].push(key)
+      end
+    end
+
+    @store = new_store
   end
 end
