bc374be3ecd2486f156c8fea95dcf96614db119d
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-02-26 12:02:04 -0800

    resizer

--- algorithms/lruCache/lib/p01_int_set.rb
+++ algorithms/lruCache/lib/p01_int_set.rb
@@ -78,18 +78,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      resize! if self.count == num_buckets - 1
+
+
+      self[num].push(num)
+      @count += 1
+    end
   end
 
   def remove(num)
+    self[num].delete(num) if include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -97,5 +107,15 @@ class ResizingIntSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    oldStore = @store
+
+    oldStore.each do |bucket|
+      bucket.each do |num|
+        new_store[num % new_store.length].push(num)
+      end
+    end
+    
+    @store = new_store
   end
 end
