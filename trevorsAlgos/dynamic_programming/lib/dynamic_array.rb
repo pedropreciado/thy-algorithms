12e8808c73685885939cfaf35b7243e0f08d2498
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-01 13:50:37 -0800

    fiish dyna arr

--- trevorsAlgos/dynamic_programming/lib/dynamic_array.rb
+++ trevorsAlgos/dynamic_programming/lib/dynamic_array.rb
@@ -5,6 +5,7 @@ class DynamicArray
 
   def initialize
     @store = []
+    @capacity = 8
   end
 
   def length
@@ -19,24 +20,33 @@ class DynamicArray
 
   # O(1)
   def []=(index, value)
-    
+    check_index(index)
+    @store[index] = value
   end
 
   # O(1)
   def pop
+    raise "index out of bounds" if length ==0
+    @store.pop
   end
 
   # O(1) ammortized; O(n) worst case. Variable because of the possible
   # resize.
   def push(val)
+    resize! if length + 1 > capacity
+    @store.push(val)
   end
 
   # O(n): has to shift over all the elements.
   def shift
+    raise "index out of bounds" if length ==0
+    @store.shift
   end
 
   # O(n): has to shift over all the elements.
   def unshift(val)
+    resize! if length + 1 > capacity
+    @store.unshift(val)
   end
 
   protected
@@ -51,5 +61,14 @@ class DynamicArray
 
   # O(n): has to copy over all the elements to the new store.
   def resize!
+    new_store = Array.new(length * 2)
+    old_store = @store
+
+    old_store.each do |el|
+      new_store.push(el)
+    end
+
+    @capacity *= 2
+    @store = new_store
   end
 end
