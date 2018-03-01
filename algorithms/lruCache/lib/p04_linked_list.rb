3ac770deaee4e44c574837c750b9c1d72d663ed7
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-01 13:14:35 -0800

    set up algos folder

--- algorithms/lruCache/lib/p04_linked_list.rb
+++ algorithms/lruCache/lib/p04_linked_list.rb
@@ -15,11 +15,16 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next = @next
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new(nil, nil)
+    @tail = Node.new(nil, nil)
+    @head.prev = nil
+    @tail.next = nil
   end
 
   def [](i)
@@ -28,12 +33,16 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    return true if @head.prev == @tail
+    false
   end
 
   def get(key)
@@ -43,15 +52,32 @@ class LinkedList
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+    new_prev = @tail.prev
+    @tail.prev = node
+    node.prev = new_prev
   end
 
   def update(key, val)
   end
 
   def remove(key)
+    node = @head
+
+    until node == @tail
+      if node.key = key
+        node.remove
+      else
+        node = node.next
+      end
+    end
   end
 
   def each
+    node = @head
+    until node == @tail
+      node = node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
