a949186276069de4cb8eb6c12053409ebe9a8045
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-17 18:20:40 -0700

    optimize space

--- algorithms/heapJS/heap_sort.js
+++ algorithms/heapJS/heap_sort.js
@@ -3,17 +3,15 @@ let BinaryMinHeap = require("./heap");
 function heapSort(array) {
   let heap = new BinaryMinHeap;
 
-  array.forEach((num) => {
-    heap.push(num);
-  })
+  while (array.length) {
+    heap.push(array.pop())
+  }
 
   let result = [];
-  console.log("1", heap.count());
+
   while (heap.count() > 0) {
     result.push(heap.extract());
-    console.log(result);
   }
-  console.log("2", heap.count());
 
   return result;
 }
